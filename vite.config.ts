efd555ec85c8ada5ed9760ff509f0907fae53f9f
Erin Meaker <erinmeaker@gmail.com>
2026-07-15 12:21:33 -0400

    re-fixing images

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolioSite/',
+  base: '/',
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     open: true,
